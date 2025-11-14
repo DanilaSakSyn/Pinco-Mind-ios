88a70eaf7a459b3939f398a6d8dff7d82ab8faf8
DanilaSakSyn <danilasak@syndicate-g.com>
2025-11-14 17:29:12 +0300

    add project and ci/cd

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,37 @@
+import 'package:app_tracking_transparency/app_tracking_transparency.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'core/services/sdk_initializer.dart';
+import 'firebase_options.dart';
 import 'package:flutter/cupertino.dart';
+import 'core/screens/splash_screen.dart';
 
-import 'app/clear_app.dart';
-
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const ClearApp());
+  WidgetsFlutterBinding.ensureInitialized().addPostFrameCallback((_) async {
+    final status = await AppTrackingTransparency.requestTrackingAuthorization();
+  });
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  SdkInitializer.prefs = await SharedPreferences.getInstance();
+  await SdkInitializer.loadRuntimeStorageToDevice();
+  var isFirstStart = !SdkInitializer.hasValue("isFirstStart");
+  var isOrganic = SdkInitializer.getValue("Organic");
+  print('add af2 $isFirstStart $isOrganic');
+  if (isFirstStart) SdkInitializer.initAppsFlyer();
+
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
 }
